4442930e2a435f64148b480f6778fdc61641c9ff
d-kjh <dkjh9942@gmail.com>
2025-05-22 16:00:42 +0900

    책 day1 ing

---
+++ src/ch02/sec01/VariableInitExample.java
@@ -0,0 +1,11 @@
+package ch02.sec01;
+
+public class VariableInitExample {
+    public static void main(String[] args) {
+        int value;
+
+        int result = value + 10;
+
+        System.out.println(result);
+    }
+}
