e953d1b9d7acd34b73d227861ea622a955f6ef25
d-kjh <dkjh9942@gmail.com>
2025-10-28 12:56:47 +0900

    10/28

---
+++ src/ch13/sec02/exam02/MyArrayList.java
@@ -0,0 +1,55 @@
+package ch13.sec02.exam02;
+
+import java.util.Arrays;
+
+public class MyArrayList<T> {
+    private T[] arr;
+
+    public MyArrayList() {
+        this.arr = (T[])new Object[0];
+    }
+
+    public int size() {
+        return arr.length;
+    }
+
+    public void add(T item) {
+        int size = size();
+        T[] newArr = (T[])new Object[size + 1];
+        newArr[size] = item;
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        this.arr = newArr;
+    }
+
+    public T get(int index) {
+        return arr[index];
+    }
+
+    public void add(int index, T item) {
+        int size = size();
+        T[] newArr = (T[])new Object[size + 1];
+        newArr[index] = item;
+        for (int i = 0; i < size; i++) {
+            newArr[i < index ? i : i + 1] = arr[i];
+        }
+        this.arr = newArr;
+    }
+
+    public T pop() {
+        int size = size() - 1;
+        T pop = arr[size];
+        T[] newArr = (T[])new Object[size];
+        for (int i = 0; i < size; i++) {
+            newArr[i] = arr[i];
+        }
+        this.arr = newArr;
+        return pop;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(arr);
+    }
+}
---
+++ src/ch13/sec02/exam02/MyArrayListTest.java
@@ -0,0 +1,31 @@
+package ch13.sec02.exam02;
+
+import java.util.Arrays;
+
+public class MyArrayListTest {
+    public static void main(String[] args) {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("가");
+        list.add("나");
+        list.add("다");
+        list.add("라");
+        list.add("마");
+
+        System.out.println(list.size());
+
+        String item = list.get(2);
+        System.out.println(item);
+
+        list.add(2, "카");
+
+        System.out.println(list.size());
+        System.out.println(list.get(2));
+
+        String lastValue = list.pop();
+        System.out.println(list.size());
+        System.out.println(lastValue);
+
+        System.out.println(list);
+    }
+}
